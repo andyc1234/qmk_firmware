cf0ee397ab429e4e69104e9cdbccd1c14322019b
Andy Chan <taichan@amazon.com>
2021-12-26 17:24:59 -0700

    updates

--- keyboards/helix/rev3_5rows/keymaps/andy/keymap.c
+++ keyboards/helix/rev3_5rows/keymaps/andy/keymap.c
@@ -16,36 +16,38 @@
 #include QMK_KEYBOARD_H
 #include "andy.h"
 
+#define TEST LT(_NAVIGATE, KC_N)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_BTN1, KC_BTN3, KC_BTN2, KC_3,    KC_4,    KC_5,                      KC_6,    KC_7, KC_8,    KC_9,    KC_0,    KC_PGUP,
     ONE_TWO, KC_Q,    KC_W,    KC_F,    KC_P,    KC_V,                      KC_J,    KC_L, KC_U,    KC_Y,    KC_SCLN, KC_PGDN,
-    ESC_SC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N, KC_E,    KC_I,    KC_O,    QUOT_SC,
+    ESC_SC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    TEST, KC_E,    KC_I,    KC_O,    QUOT_SC,
     KC_LCMD, Z_SFT,   KC_X,    KC_C,    KC_D,    KC_B,    CMD_TAB, NEW_TAB, KC_K,    KC_H, KC_COMM, KC_DOT,  SLH_SFT, KC_RCMD,
     KC_LEFT, KC_RGHT, PST_FND, CPY_CUT, TAB_NUM, SPC_CTL, BSP_ALT, TMUX,    ENT_CTL, NAVI, KC_HOME, KC_END,  KC_DOWN, KC_UP
   ),
 
   [_SHORTCUT] = LAYOUT(
-    _______, FFGPREV, FFGNEXT, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, FIREFOX, PDF,     VLC,                      _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, SLACK,   _______, CHROME,                   _______, NOTES,   EXCEL,   ITERM,   OUTLOOK, _______,
-    QUIT,    _______, _______, CHIME,   DBEAVER, PAINT,  _______, _______, _______, _______, _______, _______, _______, _______,
-    KC_VOLD, KC_VOLU, _______, _______, TAB_UP,  TAB_DN, KC_DEL,  _______, _______, _______, _______, _______, _______, _______
+    XXXXXXX, FFGPREV, FFGNEXT, XXXXXXX, XXXXXXX, XXXXXXX,                  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, FIREFOX, PDF,     VLC,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, SLACK,   XXXXXXX, CHROME,                   XXXXXXX, NOTES,   EXCEL,   ITERM,   OUTLOOK, XXXXXXX,
+    QUIT,    XXXXXXX, XXXXXXX, CHIME,   DBEAVER, PAINT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, TAB_UP,  TAB_DN, KC_DEL,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
   ),
 
   [_NUMSYM] = LAYOUT(
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F12,
     KC_CAPS, KC_LCBR, KC_RCBR, KC_HASH, KC_CIRC, KC_PERC,                   KC_BSLS, KC_7,  KC_8,    KC_9,    KC_PLUS, KC_TILD,
-    _______, KC_LPRN, KC_RPRN, KC_EXLM, KC_DLR,  KC_ASTR,                   KC_MINS, KC_4,  KC_5,    KC_6,    KC_EQL,  KC_GRV,
-    _______, KC_LBRC, KC_RBRC, KC_PIPE, KC_AMPR, KC_AT,   _______, _______, KC_UNDS, KC_1,  KC_2,    KC_3,    KC_DOT,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_0,  _______, _______, _______, _______
+    XXXXXXX, KC_LPRN, KC_RPRN, KC_EXLM, KC_DLR,  KC_ASTR,                   KC_MINS, KC_4,  KC_5,    KC_6,    KC_EQL,  KC_GRV,
+    XXXXXXX, KC_LBRC, KC_RBRC, KC_PIPE, KC_AMPR, KC_AT,   XXXXXXX, XXXXXXX, KC_UNDS, KC_1,  KC_2,    KC_3,    KC_DOT,  XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_0,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
   ),
 
   [_NAVIGATE] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-    _______, _______, LNSTART, KC_UP,   LNEND,   _______,                   _______, _______, _______, _______, _______, _______,
-    _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,                   _______, KC_LSFT, KC_LSFT, _______, _______, _______,
-    _______, _______, WNDPREV, XXXXXXX, WNDNEXT, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, LNSTART, KC_UP,   LNEND,   XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,                   XXXXXXX, KC_LSFT, KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, WNDPREV, XXXXXXX, WNDNEXT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
   ),
 };
--- users/andy/andy.c
+++ users/andy/andy.c
@@ -7,55 +7,55 @@ bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case CMD_TAB:
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(LCMD(KC_TAB));
-            } else if (record->event.pressed) {
-                tap_code16(LCMD(KC_GRV));
-            }
-            return false;
-            break;
-        case NEW_TAB:
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(LCMD(KC_T));
-            } else if (record->event.pressed) {
-                tap_code16(LALT(KC_HOME));
-            }
-            return false;
-            break;
-        case CPY_CUT:
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(LCMD(KC_C));
-            } else if (record->event.pressed) {
-                tap_code16(LCMD(KC_X));
-            }
-            return false;
-            break;
-        case PST_FND:
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(LCMD(KC_V));
-            } else if (record->event.pressed) {
-                tap_code16(LCMD(KC_F));
-            }
-            return false;
-            break;
-        case TMUX:
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(LCTL(KC_SPC));
-            } else if (record->event.pressed) {
-                tap_code16(LCTL(KC_SPC));
-                tap_code16(KC_LBRC);
-            }
-            return false;
-            break;
-        case ONE_TWO:
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_1);
-            } else if (record->event.pressed) {
-                tap_code16(KC_2);
-            }
-            return false;
-            break;
+    case CMD_TAB:
+        if (record->tap.count && record->event.pressed) {
+            tap_code16(LCMD(KC_TAB));
+        } else if (record->event.pressed) {
+            tap_code16(LCMD(KC_GRV));
+        }
+        return false;
+        break;
+    case NEW_TAB:
+        if (record->tap.count && record->event.pressed) {
+            tap_code16(LCMD(KC_T));
+        } else if (record->event.pressed) {
+            tap_code16(LALT(KC_HOME));
+        }
+        return false;
+        break;
+    case CPY_CUT:
+        if (record->tap.count && record->event.pressed) {
+            tap_code16(LCMD(KC_C));
+        } else if (record->event.pressed) {
+            tap_code16(LCMD(KC_X));
+        }
+        return false;
+        break;
+    case PST_FND:
+        if (record->tap.count && record->event.pressed) {
+            tap_code16(LCMD(KC_V));
+        } else if (record->event.pressed) {
+            tap_code16(LCMD(KC_F));
+        }
+        return false;
+        break;
+    case TMUX:
+        if (record->tap.count && record->event.pressed) {
+            tap_code16(LCTL(KC_SPC));
+        } else if (record->event.pressed) {
+            tap_code16(LCTL(KC_SPC));
+            tap_code16(KC_LBRC);
+        }
+        return false;
+        break;
+    case ONE_TWO:
+        if (record->tap.count && record->event.pressed) {
+            tap_code16(KC_1);
+        } else if (record->event.pressed) {
+            tap_code16(KC_2);
+        }
+        return false;
+        break;
     }
     return process_record_keymap(keycode, record);
 }
